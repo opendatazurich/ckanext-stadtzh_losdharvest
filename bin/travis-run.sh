59ee3ec366f6d094bbb0151742ad7b2f76787487
Rae Knowler <rae.knowler@liip.ch>
2020-10-27 18:00:10 +0100

    Add isort to travis-build.sh and sort existing imports

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -10,6 +10,8 @@ trap "cleanup" EXIT
 
 # Check PEP-8 code style and McCabe complexity
 flake8 --statistics --show-source ckanext
+# Check imports are sorted
+isort -c -rc ckanext
 
 # run tests
 nosetests --ckan --with-pylons=subdir/test.ini --verbose ckanext/stadtzh_losdharvest
