6bedfddb1fa490c38c6f39424041f5c7848b340e
Rae Knowler <rae.knowler@liip.ch>
2020-10-27 15:55:29 +0100

    Base harvester on DCATRDFHarvester

--- ckanext/stadtzh_losdharvest/harvester.py
+++ ckanext/stadtzh_losdharvest/harvester.py
@@ -1,21 +1,20 @@
 # coding: utf-8
 
-from ckanext.harvest.harvesters import HarvesterBase
-from pylons import config
+import ckan.plugins as p
+
+from ckanext.dcat.harvesters.rdf import DCATRDFHarvester
+from ckanext.dcat.interfaces import IDCATRDFHarvester
 import logging
 log = logging.getLogger(__name__)
 
 
-class StadtzhLosdHarvester(HarvesterBase):
+class StadtzhLosdHarvester(DCATRDFHarvester):
     """
     LOSD harvester for the City of Zürich
     """
-    def __init__(self, **kwargs):
-        HarvesterBase.__init__(self, **kwargs)
-        try:
-            self.CKAN_SITE_URL = config['ckan.site_url']
-        except KeyError as e:
-            raise Exception("'%s' not found in config" % e.message)
+    p.implements(IDCATRDFHarvester, inherit=True)
+
+    harvest_job = None
 
     def info(self):
         return {
@@ -23,15 +22,3 @@ class StadtzhLosdHarvester(HarvesterBase):
             'title': 'LOSD Harvester for the City of Zurich',
             'description': 'Harvester for the LOSD Portal of the City of Zurich'  # noqa
         }
-
-    def gather_stage(self, harvest_job):
-        log.debug('In StadtzhLosdHarvester gather_stage')
-        return True
-
-    def fetch_stage(self, harvest_object):
-        log.debug('In StadtzhLosdHarvester fetch_stage')
-        return True
-
-    def import_stage(self, harvest_object):
-        log.debug('In StadtzhLosdHarvester import_stage')
-        return True
